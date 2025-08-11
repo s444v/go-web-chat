667db5de316baa7675f122dbccd4fabb45dea80f
s444v <dymnich02@gmail.com>
2025-08-11 21:48:09 +0300

    fix

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -9,9 +9,10 @@ const LIMIT = 50
 func HandlersInit(router *gin.Engine) {
 	router.NoRoute(func(c *gin.Context) {
 		c.File("./web/index.html")
+		//c.File("./web/login.html")
 	})
 	router.POST("/login", signinHandler)
-	auth := router.Group("/api", authCookieMiddleware())
+	auth := router.Group("/", authCookieMiddleware())
 	//router.Static("/static", "./web/static")
 	auth.GET("/users", getUsers)
 }
